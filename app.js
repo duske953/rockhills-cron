1282d0e62f908bbf3679f2ebfe4e7b0317d78e52
Eloho Kennedy <kennyduske@gmail.com>
2025-08-03 07:54:16 +0100

    add variable port

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/', async (req, res) => {
   res.send('okay');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('This port is listening');
 });
